a853ec552cfb803810d8121191a498fad3eed0fa
Lancelcode <127974834+Lancelcode@users.noreply.github.com>
2024-04-23 12:03:36 +0100

    base app for oop class

---
+++ App.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Student 
+{
+
+   private String studentID;
+   private String surname;
+   private String firstname;
+   private String course;
+   private String email;
+   private int dob;
+   private int mobileNumber;
+
+   private void student(){}
+   // This is the constructor of the class Interface
+   private void studentID(){}
+
+   // Assign the age of the Employee  to the variable age.
+   private void surname(){}
+
+   /* Assign the designation to the variable designation.*/
+   private void firstname(){}
+
+   /* Assign the salary to the variable	salary.*/
+   private void course(){}
+
+   private void email(){}
+
+   private void dob(){}
+
+   private void mobileNumber(){}
+
+
+
+   /* Print the Student details */
+   public void printStudent() {
+      System.out.println("Name:"+ name );
+      System.out.println("Age:" + age );
+      System.out.println("Designation:" + designation );
+      System.out.println("Salary:" + salary);
+   }
+}
